97337e0c7884ce87789a37cd3409189dcca3bbf7
aryandhirhi004 <aryandhirhi004@gmail.com>
2025-04-29 21:49:24 +0530

    feat: connected to telegram

---
+++ research/tele_echo_bot.py
@@ -0,0 +1,52 @@
+import logging
+import os
+import sys
+import asyncio
+
+from dotenv import load_dotenv
+from aiogram import Bot, Dispatcher, html
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+load_dotenv()
+API_TOKEN = os.getenv("TELEGRAM_TOKEN")
+
+logging.basicConfig(level=logging.INFO)
+
+# Initializing bot and dispaticher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    """
+    This handler receives messages with `/start` command
+    """
+    await message.reply(f"Hello! chatbot by Aryan")
+
+
+@dp.message()
+async def echo_handler(message: Message) -> None:
+    try:
+        # Send a copy of the received message
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        # But not all the types is supported to be copied so need to handle it
+        await message.answer("Nice try!")
+
+
+
+async def main() -> None:
+    # Initialize Bot instance with default bot properties which will be passed to all API calls
+    bot = Bot(token=API_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+    # And the run events dispatching
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
